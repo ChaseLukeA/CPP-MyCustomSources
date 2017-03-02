bdadd6fecf655ad624f32218f792764b912bd5c5
ChaseLukeA <chase.luke.a@gmail.com>
2017-03-02 10:37:04 -0600

    Add custom Queue and Stack libraries

---
+++ include/Queue.h
@@ -0,0 +1,53 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <list>
+
+using namespace std;
+
+template<class T>
+class Queue
+{
+    private:
+        list<T> m_List;
+
+    public:
+        Queue() {};
+        virtual ~Queue() {};
+
+        // check if the Queue is empty
+        bool isEmpty()
+        {
+            return m_List.empty();
+        }
+
+        // get the number of T's in the Queue
+        int size()
+        {
+            return m_List.size();
+        }
+
+        // get the T at the beginning of the Queue
+        T front()
+        {
+            return *(m_List.begin());
+        }
+
+        //add a T to the end of the Queue
+        bool enQueue(T t)
+        {
+            m_List.push_back(t);
+            return true;
+        };
+
+        // remove the T from the beginning of the Queue
+        bool deQueue()
+        {
+            if (m_List.empty()) return false;
+
+            m_List.pop_front();
+            return true;
+        };
+};
+
+#endif // QUEUE_H
---
+++ include/Stack.h
@@ -0,0 +1,53 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <list>
+
+using namespace std;
+
+template<class T>
+class Stack
+{
+    private:
+        list<T> m_List;
+
+    public:
+        Stack() {};
+        virtual ~Stack() {};
+
+        // check if the Stack is empty
+        bool isEmpty()
+        {
+            return m_List.empty();
+        }
+
+        // get the number of T's in the Stack
+        int size()
+        {
+            return m_List.size();
+        }
+
+        // get the T at the top of the Stack
+        T top()
+        {
+            return *(m_List.begin());
+        }
+
+        // add a T to the top of the Stack
+        bool push(T t)
+        {
+            m_List.push_front(t);
+            return true;
+        };
+
+        // remove the T from the top of the Stack
+        bool pop()
+        {
+            if (m_List.empty()) return false;
+
+            m_List.pop_front();
+            return true;
+        };
+};
+
+#endif // STACK_H
